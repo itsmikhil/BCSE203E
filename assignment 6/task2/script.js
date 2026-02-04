988f23942812b6f5296a427a1ccbb054d4bd3be5
itsmikhil <ailani.mikhil@gmail.com>
2026-02-04 11:22:29 +0530

    built displayTime element

---
+++ assignment 6/task2/script.js
@@ -0,0 +1,12 @@
+let textBox=document.querySelector(".time")
+
+let displayTime=()=>{
+    let now=new Date();
+    let hrs=now.getHours();
+    let mins=now.getMinutes();
+    textBox.innerHTML= `Current time is ${hrs}:${mins}`;
+    console.log("refresh");
+    
+}
+displayTime();
+setInterval(displayTime, 60000);
