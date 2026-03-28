ccbcc119f64d0f34cb707fae86f2a2aed695d935
itsmikhil <ailani.mikhil@gmail.com>
2026-03-28 23:10:12 +0530

    completed task2

--- react-practice/task2/src/App.jsx
+++ react-practice/task2/src/App.jsx
@@ -1,121 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import importedData from "./students.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [data, setdata] = useState(importedData);
+  const [showdata, setshowdata] = useState(importedData);
+  const [show, setshow] = useState("all");
+  const [search, setsearch] = useState("");
+
+  const addFields = () => {
+    let result = data.map((el) => {
+      let total = Number(el.marks.Math + el.marks.Science + el.marks.English);
+      let avg = Number((total / 3).toFixed(2));
+      return { ...el, total, avg, status: avg >= 75 ? "Pass" : "Fail" };
+    });
+    setshowdata(result);
+    setdata(result);
+  };
+
+  const handleSearch = (e) => {
+    const input = e.target.value;
+    if (input === "") {
+      setsearch(input);
+      setshowdata(data);
+      addFields();
+      return;
+    }
+    setsearch(input);
+    setshowdata((prev) => prev.filter((el) => el.name.includes(input)));
+  };
+
+  const sortByName = () => {
+    setshowdata((prev) =>
+      [...prev].sort((a, b) => a.name.localeCompare(b.name)),
+    );
+  };
+
+  const sortByTotal = () => {
+    // we have to ...prev so that new array is returned and accepted
+    // otherwise on it returns same aaray so react doesnt accept it
+    setshowdata((prev) => [...prev].sort((a, b) => b.total - a.total));
+  };
+
+  const handleFilter = (val) => {
+    if (val === "all") {
+      addFields();
+      setshow("all");
+      return;
+    }
+    setshow(val);
+    setshowdata(data.filter((el) => el.status.toLowerCase() === val));
+  };
+
+  useEffect(() => {
+    addFields();
+  }, []);
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+      <h3>Student Report Dashboard</h3>
+      <input value={search} onChange={(e) => handleSearch(e)} type="text" />
+      <select
+        value={show}
+        onChange={(e) => handleFilter(e.target.value)}
+        name="show"
+        id=""
+      >
+        <option value="all">All</option>
+        <option value="pass">Pass</option>
+        <option value="fail">Fail</option>
+      </select>
+      <button onClick={sortByName}>Sort By Name</button>
+      <button onClick={sortByTotal}>Sort By Total</button>
+      <table>
+        <tr>
+          <th>Roll No</th>
+          <th>Name</th>
+          <th>Class</th>
+          <th>Math</th>
+          <th>Science</th>
+          <th>English</th>
+          <th>Total</th>
+          <th>Average</th>
+          <th>Result</th>
+        </tr>
+        {showdata.map((el) => {
+          return (
+            <tr>
+              <td>{el.roll}</td>
+              <td>{el.name}</td>
+              <td>{el.class}</td>
+              <td>{el.marks.Math}</td>
+              <td>{el.marks.Science}</td>
+              <td>{el.marks.English}</td>
+              <td>{el.total}</td>
+              <td>{el.avg}</td>
+              <td>{el.status}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
+};
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
-
-export default App
+export default App;
